c93d0089e3301b2fbb007986c5fe05f4093f93e5
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-03-24 11:01:55 -0700

    fixes

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,9 @@ const index_1 = __importDefault(require("./routes/index"));
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+const location = path_1.default.join(__dirname, "uploads");
+console.log(location);
 dotenv_1.default.config({ path: "./config.env" });
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
@@ -15,7 +18,7 @@ app.use((0, cors_1.default)());
 if (process.env.NODE_ENV === "development") {
     app.use((0, morgan_1.default)("dev"));
 }
-app.use("/uploads", express_1.default.static("uploads"));
+app.use(location, express_1.default.static("uploads"));
 app.use(express_1.default.json());
 app.use(index_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import routes from "./routes/index";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import path from "path";
+
+const location = path.join(__dirname, "uploads");
+
+console.log(location);
 
 dotenv.config({ path: "./config.env" });
 
@@ -12,7 +17,7 @@ app.use(cors());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use("/uploads", express.static("uploads"));
+app.use(location, express.static("uploads"));
 
 app.use(express.json());
 
