43a60d402222f0f9b391b15d8fa98afb87fcab90
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-03-26 12:54:02 -0700

    fixes-12

--- dist/app.js
+++ dist/app.js
@@ -8,29 +8,28 @@ const index_1 = __importDefault(require("./routes/index"));
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// import path from "path";
 const app = (0, express_1.default)();
 dotenv_1.default.config({ path: "./config.env" });
-app.use((0, cors_1.default)());
 // Configure CORS
-// const allowedOrigins = [
-//   "http://localhost:5174",
-//   "https://r28.ng",
-//   "http://r28.e37digital.com",
-// ];
-// const corsOptions: cors.CorsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true,
-//   optionsSuccessStatus: 204,
-// };
-// app.use(cors(corsOptions));
+const allowedOrigins = [
+    "http://localhost:5174",
+    "https://r28.ng",
+    "http://r28.e37digital.com",
+];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+app.use((0, cors_1.default)(corsOptions));
 // Middleware
 if (process.env.NODE_ENV === "development") {
     app.use((0, morgan_1.default)("dev"));
@@ -40,5 +39,5 @@ app.use("/uploads", express_1.default.static("uploads"));
 app.use(express_1.default.json());
 app.use(index_1.default);
 // Handle preflight requests
-// app.options("*", cors(corsOptions));
+app.options("*", (0, cors_1.default)(corsOptions));
 exports.default = app;
