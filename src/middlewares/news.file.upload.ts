1d8c3f016a37fa55aa4dfbc50e4dbcfc60f4739a
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-03-26 13:24:18 -0700

    fixes-17

--- src/middlewares/news.file.upload.ts
+++ src/middlewares/news.file.upload.ts
@@ -1,9 +1,43 @@
+// import multer from "multer";
+// import path from "path";
+
+// export const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     let ext = path.extname(file.originalname);
+//     cb(null, Date.now() + ext); // Use the original filename
+//   },
+// });
+
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 export const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    const uploadDir = "uploads/";
+
+    fs.access(uploadDir, (err) => {
+      if (err && err.code === "ENOENT") {
+        fs.mkdir(uploadDir, { recursive: true }, (mkdirErr) => {
+          if (mkdirErr) {
+            console.error("Error creating uploads folder:", mkdirErr);
+            // Provide a default destination if error occurs
+            cb(null, "default_destination/");
+          } else {
+            cb(null, uploadDir);
+          }
+        });
+      } else if (err) {
+        console.error("Error accessing uploads folder:", err);
+        // Provide a default destination if error occurs
+        cb(null, "default_destination/");
+      } else {
+        cb(null, uploadDir);
+      }
+    });
   },
   filename: function (req, file, cb) {
     let ext = path.extname(file.originalname);
