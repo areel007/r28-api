46b507cd8024c2c8cef912d8c2110280c2619c26
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-04-23 14:10:05 -0700

    fix image disappearance

--- dist/middlewares/news.file.upload.js
+++ dist/middlewares/news.file.upload.js
@@ -1,20 +1,9 @@
 "use strict";
-// import multer from "multer";
-// import path from "path";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.storage = void 0;
-// export const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     let ext = path.extname(file.originalname);
-//     cb(null, Date.now() + ext); // Use the original filename
-//   },
-// });
 const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
--- src/middlewares/news.file.upload.ts
+++ src/middlewares/news.file.upload.ts
@@ -1,16 +1,3 @@
-// import multer from "multer";
-// import path from "path";
-
-// export const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     let ext = path.extname(file.originalname);
-//     cb(null, Date.now() + ext); // Use the original filename
-//   },
-// });
-
 import multer from "multer";
 import path from "path";
 import fs from "fs";
